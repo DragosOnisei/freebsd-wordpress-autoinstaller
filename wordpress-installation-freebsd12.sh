311807c1ec56ffd5b4883faf257d008467018f70
Dragos Onisei <33819238+DragosOnisei@users.noreply.github.com>
2023-07-15 20:00:45 +0300

    Update wordpress-installation-freebsd12.sh
    V1

--- wordpress-installation-freebsd12.sh
+++ wordpress-installation-freebsd12.sh
@@ -375,13 +375,13 @@ cat << 'EOF_WPCONFIG' | cat > /usr/local/www/apache24/data/wp-config.php
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '${DB_WPDB_NAME}' );
+define( 'DB_NAME', 'database_name_here' );
 
 /** MySQL database username */
-define( 'DB_USER', '${DB_WPDB_USER}' );
+define( 'DB_USER', 'username_here' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '${DB_WPDB_USER_PASSWORD}' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
