18b60a67a5fa0c9bbcb0e5aaea36cec52959037a
Dragos Onisei <33819238+DragosOnisei@users.noreply.github.com>
2023-09-04 19:26:58 +0300

    Update wordpress-installation-freebsd12.sh
    2

--- wordpress-installation-freebsd12.sh
+++ wordpress-installation-freebsd12.sh
@@ -1,100 +1,119 @@
 #!/usr/local/bin/bash
 
-# Set the colors
+printf "\n"
+
+## Set the colors ##
 NC='\033[0m'
 GREEN='\033[0;32m'
-
-printf "\n"
 
 if [[ $USER != root ]]; then
     printf "You are not root!!! Log in as root, please.\n"
     exit
 fi
 
-if [[ ${SHELL} != $(which bash) ]] && [[ ${SHELL} != /usr/local/bin/bash ]] && [[ ${SHELL} != /bin/bash ]]; then
-    printf "This is not bash! Installing and setting bash as your default shell, re-login and start the script again.\n"
-    pkg install -y bash &> /dev/null
-    chsh -s bash root
-    exit
-fi
+printf "Installing and configuring software:... \n"
 
-printf "Installing and configuring software:... "
-
-# Pre-Install the software required for basic jail setup
+## Pre-Install the software required for basic jail stuff ##
 pkg install -y nano &> /dev/null
-pkg install -y rsync curl pwgen
-pkg install -y mod_php80
+pkg install -y mod_php80 php80-mysqli php80-tokenizer php80-zlib php80-zip php80 rsync php80-gd curl php80-curl php80-xml php80-bcmath php80-mbstring php80-pecl-imagick php80-pecl-imagick-im7 php80-iconv php80-filter php80-pecl-json_post php80-pear-Services_JSON php80-exif php80-fileinfo php80-dom php80-session php80-ctype php80-simplexml php80-phar php80-gmp &> /dev/null
+pkg install -y apache24 mariadb105-server mariadb105-client
 
-# Install MariaDB 10.5
-pkg install -y mariadb105-server mariadb105-client
+sysrc apache24_enable=yes mysql_enable=yes &> /dev/null
+service apache24 start &> /dev/null
+service mysql-server start &> /dev/null
 
-# Enable and start MariaDB on boot
-sysrc mysql_enable="YES"
-service mysql-server start
+pkg update -fq &> /dev/null
+pkg upgrade -y &> /dev/null
+pkg install -y nano htop bmon iftop pwgen sudo figlet &> /dev/null
 
-# Secure the MariaDB installation
-mysql_secure_installation <<EOF_MYSQL_SECURE
+printf "."
+
+## Set the correct banner ##
+figlet GATEWAY - IT > /etc/motd
+service motd restart &> /dev/null
+
+## Secure the MariaDB install ##
+mysql_secure_installation <<EOF_MSQLSI &> /dev/null
+
 n
 y
 y
 y
 y
-EOF_MYSQL_SECURE
+EOF_MSQLSI
 
-# Install Apache
-pkg install -y apache24
+printf "."
 
-# Enable and start Apache on boot
-sysrc apache24_enable="YES"
-service apache24 start
+## Download the latest version of WordPress and move it into the correct folder with the right permissions ##
+cd /tmp
 
-# Install required PHP extensions
-pkg install -y php80-mysqli php80-tokenizer php80-zlib php80-zip php80-gd php80-curl php80-xml php80-bcmath php80-mbstring php80-pecl-imagick php80-pecl-imagick-im7 php80-iconv php80-filter php80-pecl-json_post php80-pear-Services_JSON php80-exif php80-fileinfo php80-dom php80-session php80-ctype php80-simplexml php80-phar php80-gmp
+if [[ ! -f /tmp/latest.tar.gz ]]; then
+    curl -s https://wordpress.org/latest.tar.gz -o latest.tar.gz -Y 10000 -y 10
+fi
 
-printf "${GREEN}Done${NC}\n"
-printf "Configuring Apache:..."
+tar xf /tmp/latest.tar.gz
 
-# Update the Apache configuration file
-sed -i '' -e 's/#LoadModule php_module libexec\/apache24\/libphp.so/LoadModule php_module libexec\/apache24\/libphp.so/' /usr/local/etc/apache24/httpd.conf
+printf "."
 
-# Add PHP file handling to Apache configuration
-cat <<EOF_APACHE_PHP >> /usr/local/etc/apache24/httpd.conf
+rm /usr/local/www/apache24/data/index.html
 
-<IfModule dir_module>
-    DirectoryIndex index.php index.html
-    <FilesMatch "\.php$">
-        SetHandler application/x-httpd-php
-    </FilesMatch>
-    <FilesMatch "\.phps$">
-        SetHandler application/x-httpd-php-source
-    </FilesMatch>
-</IfModule>
-EOF_APACHE_PHP
+cp -r /tmp/wordpress/* /usr/local/www/apache24/data/
+chown -R www:www /usr/local/www/apache24/data/
 
-# Restart Apache
-service apache24 restart
+## .htaccess file + some php.ini configuration settings inside it ##
+touch /usr/local/www/apache24/data/.htaccess &> /dev/null
+chown www:www /usr/local/www/apache24/data/.htaccess
 
-printf "${GREEN}Done${NC}\n"
-printf "Downloading WordPress and setting up configuration:..."
+echo "#PHP.INI VALUES" >> /usr/local/www/apache24/data/.htaccess
+echo "php_value upload_max_filesize 500M" >> /usr/local/www/apache24/data/.htaccess
+echo "php_value post_max_size 500M" >> /usr/local/www/apache24/data/.htaccess
+echo "php_value memory_limit 256M" >> /usr/local/www/apache24/data/.htaccess
+echo "php_value max_execution_time 300" >> /usr/local/www/apache24/data/.htaccess
+echo "php_value max_input_time 300" >> /usr/local/www/apache24/data/.htaccess
 
-# Download and install WP-CLI
-cd /root/
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &> /dev/null
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+printf "."
 
-# Install WordPress
-cd /usr/local/www/
-wp core download --allow-root
-wp config create --dbname='wordpress' --dbuser='wpuser' --dbpass='wppassword' --allow-root
-wp core install --url='http://localhost' --title='My WordPress Site' --admin_user='admin' --admin_password='admin' --admin_email='admin@example.com' --allow-root
+## Create a custom wp-config.php file with the correct database and salts configuration ##
+WP_DB_PREFIX="wp_$(pwgen 10 1 --no-capitalize --no-numerals)"
+WP_SALT1="$(pwgen 64 1)"
+WP_SALT2="$(pwgen 64 1)"
+WP_SALT3="$(pwgen 64 1)"
+WP_SALT4="$(pwgen 64 1)"
+WP_SALT5="$(pwgen 64 1)"
+WP_SALT6="$(pwgen 64 1)"
+WP_SALT7="$(pwgen 64 1)"
+WP_SALT8="$(pwgen 64 1)"
 
-# Set file permissions
-chown -R www:www /usr/local/www/
+cp /usr/local/www/apache24/data/wp-config-sample.php /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'DB_NAME'/s/'[^']*'/'your_database_name_here'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'DB_USER'/s/'[^']*'/'your_username_here'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'DB_PASSWORD'/s/'[^']*'/'your_password_here'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'DB_HOST'/s/'[^']*'/'localhost'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/table_prefix/p" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/table_prefix/s/wp_/${WP_DB_PREFIX}/g" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'AUTH_KEY',/s/'[^']*'/'${WP_SALT1}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'SECURE_AUTH_KEY',/s/'[^']*'/'${WP_SALT2}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'LOGGED_IN_KEY',/s/'[^']*'/'${WP_SALT3}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'NONCE_KEY',/s/'[^']*'/'${WP_SALT4}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'AUTH_SALT',/s/'[^']*'/'${WP_SALT5}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'SECURE_AUTH_SALT',/s/'[^']*'/'${WP_SALT6}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'LOGGED_IN_SALT',/s/'[^']*'/'${WP_SALT7}'/" /usr/local/www/apache24/data/wp-config.php
+sed -i '' "/define( 'NONCE_SALT',/s/'[^']*'/'${WP_SALT8}'/" /usr/local/www/apache24/data/wp-config.php
 
-printf "${GREEN}Done${NC}\n"
+printf "."
 
-printf "\n"
-printf "The installation is now finished. You can access your WordPress site by visiting ${GREEN}http://localhost${NC} in your web browser.\n"
-printf "You can log in to the WordPress admin dashboard with the username ${GREEN}admin${NC} and the password ${GREEN}admin${NC}.\n"
+## Initialize new WordPress website and remove the default content ##
+sudo -u www wp core install --url=http://127.0.0.1 --title="Dragos Created Website" --admin_user=admin --admin_password=admin --admin_email=dragosonisei@gmail.com &> /dev/null
+sudo -u www wp site empty --yes &> /dev/null
+
+printf " .. ${GREEN}Done${NC}\n"
+
+## Restart apache and ensure it's running ##
+service apache24 restart &> /dev/null
+
+IPADDR=$(ifconfig | grep "192\|10\|172" | awk '{print $2}' | awk '/^192|^10|^172/')
+
+printf "The installation is now finished. In case you forgot, this VM IP is: ${GREEN}${IPADDR}${NC}\n"
+printf "Go to ${GREEN}http://${IPADDR}/wp-admin/${NC} if you'd like to configure or test your new WordPress website.\n"
+
 printf "\n"
