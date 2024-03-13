73a7719e4cb69968cb07ad5972fa6ae946fdfd2c
Dragos Onisei <33819238+DragosOnisei@users.noreply.github.com>
2024-03-13 15:22:49 +0200

    Update wordpress-installation-freebsd12.sh

--- wordpress-installation-freebsd12.sh
+++ wordpress-installation-freebsd12.sh
@@ -456,21 +456,20 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
+
 // define('DISABLE_WP_CRON', true);
 define('WP_DEBUG', false);
 
-define('WP_SITEURL', 'https://'.$_SERVER['HTTP_HOST']);
-define('WP_HOME', 'https://'.$_SERVER['HTTP_HOST']);
-define('FORCE_SSL_ADMIN', true);
-
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
-$_SERVER['HTTPS']='on';
-
-// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
+    $_SERVER['HTTPS'] = 'on';
+    define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
+    define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
+} else {
+    define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+    define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
 }
+
+define( 'WP_CACHE', true );
 
 /* That's all, stop editing! Happy publishing. */
 
